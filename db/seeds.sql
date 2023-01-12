1b80f7761443dfb312a860c66c75985d3de06b7e
masonMO25 <masonneale@gmail.com>
2023-01-12 14:11:37 -0600

    added seeds and index information

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,3 @@
--- Inserts names of departments into department table
 INSERT INTO department
   (name)
 VALUES
@@ -7,7 +6,6 @@ VALUES
   ('Finance'),
   ('Legal');
 
--- Inserts roles of employee into role table
 INSERT INTO role
   (title, salary, department_id)
 VALUES
@@ -16,11 +14,10 @@ VALUES
   ('Accountant', 100000, 3),
   ('Lawyer', 150000, 4);
 
--- Inserts employee information into employee table
 INSERT INTO employee
   (first_name, last_name, role_id, manager_id)
 VALUES
   ('Mickey', 'Mouse', 1, 3),
-  ('James', 'CAmeron', 2, 4),
+  ('James', 'Cameron', 2, 4),
   ('Joe', 'Mama', 3, 5),
   ('Molly', 'Morgan', 4, 1);
