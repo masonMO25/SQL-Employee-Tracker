c04851110dbc04ebe32b8f04e2a6c431e2dfbd2b
masonMO25 <masonneale@gmail.com>
2023-01-12 18:04:21 -0600

    fixed seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,17 @@
-INSERT INTO department
-  (name)
-VALUES
-  ('Engineering'),
-  ('Sales'),
-  ('Finance'),
-  ('Legal');
+INSERT INTO department (name)
+VALUES  ('Engineering'),
+        ('Sales'),
+        ('Finance'),
+        ('Legal');
 
-INSERT INTO role
-  (title, salary, department_id)
-VALUES
-  ('Software Engineer', 70000, 1),
-  ('Salesperson', 70000, 2),
-  ('Accountant', 100000, 3),
-  ('Lawyer', 150000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Software Engineer', 70000, 1),
+        ('Salesperson', 70000, 2),
+        ('Accountant', 100000, 3),
+        ('Lawyer', 150000, 4);
 
-INSERT INTO employee
-  (first_name, last_name, role_id, manager_id)
-VALUES
-  ('Mickey', 'Mouse', 1, 3),
-  ('James', 'Cameron', 2, 4),
-  ('Joe', 'Mama', 3, 5),
-  ('Molly', 'Morgan', 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Mickey', 'Mouse', 1, 3),
+        ('James', 'Cameron', 2, 4),
+        ('Joe', 'Mama', 3, 5),
+        ('Molly', 'Morgan', 4, 1);
