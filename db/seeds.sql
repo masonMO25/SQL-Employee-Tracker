6d0a170a18bab15d146e64c75b596223d9a6b106
masonMO25 <masonneale@gmail.com>
2023-03-21 15:44:37 -0500

    spelling

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,16 @@
-INSERT INTO departments (name)
+INSERT INTO department (name)
 VALUES  ('Engineering'),
         ('Sales'),
         ('Finance'),
         ('Legal');
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES  ('Software Engineer', 70000, 1),
         ('Salesperson', 70000, 2),
         ('Accountant', 100000, 3),
         ('Lawyer', 150000, 4);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES  ('Mickey', 'Mouse', 1, 3),
         ('James', 'Cameron', 2, 4),
         ('Joe', 'Mama', 3, 5),
